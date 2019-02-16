eedc560a4ff9d1f15fac2e082f2826cf63b4164f
ifotn <rich@infrontofthenet.com>
2019-02-16 12:24:37 -0500

    Add project files.

---
+++ COMP2084MidtermW2019/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2084MidtermW2019.MvcApplication" Language="C#" %>
---
+++ COMP2084MidtermW2019/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2084MidtermW2019.Startup))]
+namespace COMP2084MidtermW2019
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
